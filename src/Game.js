4c9b41a8cb57053da2e34fd990b9265b24c09abd
NDBrewer20 <ndbrewer@mtu.edu>
2026-02-25 23:07:17 -0500

    Added Game

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,42 @@
-function Game() {
-    return (
-        <div>
-            <h1>Unity Game Loads Here</h1>
-        </div>
-    );
-}
+import React, { useEffect } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+const Game = () => {
+	const {
+		unityProvider,
+		isLoaded,
+		addEventListener,
+		removeEventListener
+	} = useUnityContext({
+		loaderUrl: "/UnityBuild/Build/Obstacle-Avoidance_Build.loader.js",
+		dataUrl: "/UnityBuild/Build/Obstacle-Avoidance_Build.data",
+		frameworkUrl: "/UnityBuild/Build/Obstacle-Avoidance_Build.framework.js",
+		codeUrl: "/UnityBuild/Build/Obstacle-Avoidance_Build.wasm",
+	});
+
+	useEffect(() => {
+		const handleOnTrialEnd = (isWin) => {
+			if (isWin) {
+				alert("DEBUG: SAFE");
+			} else {
+				alert("DEBUG: COLLISION");
+			}
+		};
+
+		// Attach the listener
+		addEventListener("OnTrialEnd", handleOnTrialEnd);
+
+		// Clean up when the user leaves the Game page
+		return () => {
+			removeEventListener("OnTrialEnd", handleOnTrialEnd);
+		};
+	}, [addEventListener, removeEventListener]);
+
+	return (
+		<div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+			<Unity unityProvider={unityProvider} style={{ width: "90vw", height: "90vh" }} />
+		</div>
+	);
+};
 
 export default Game;
